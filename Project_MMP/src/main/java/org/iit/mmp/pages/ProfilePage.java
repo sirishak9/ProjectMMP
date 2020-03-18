1a18155e56a4eafd5cba57b4945c40f6c106ad09
siriseleniun <sirishakotha9@gmail.com>
2020-03-17 23:55:02 -0700

    Updated my code

--- Project_MMP/src/main/java/org/iit/mmp/pages/ProfilePage.java
+++ Project_MMP/src/main/java/org/iit/mmp/pages/ProfilePage.java
@@ -1,49 +1,102 @@
 package org.iit.mmp.pages;
 
+import java.util.HashMap;
+import java.util.Random;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.Select;
 
 public class ProfilePage {
-	WebDriver driver;
-	public ProfilePage(WebDriver driver)
-	{
+	private WebDriver driver;
+
+	public ProfilePage(WebDriver driver) {
 		this.driver = driver;
 	}
-	public void profileTab() throws Exception {
+
+	public void profile() throws InterruptedException {
+
+		Random rd = new Random();
+		HashMap<String, String> hMap = new HashMap<String, String>();
+
+		// click the profile menu
+		driver.findElement(By.xpath("//span[contains(text(),'Profile']")).click();
+
+		// title
+		driver.findElement(By.className("panel-title"));
+
+		// fname
+		WebElement fn = driver.findElement(By.id("fname"));
+		fn.sendKeys("FirstName" + (char) (65 + rd.nextInt(26)));
+		hMap.put("Fname", fn.getAttribute("Value"));
+
+		// lname
+		WebElement ln = driver.findElement(By.id("lname"));
+		ln.sendKeys("LastName" + (char) (65 + rd.nextInt(26)));
+		hMap.put("Lname", ln.getAttribute("Value"));
+
+		// licence
+		WebElement license = driver.findElement(By.id("licn"));
+		int lisendvalue = rd.nextInt(999999999);
+		license.sendKeys("" + lisendvalue);
+		hMap.put("License", license.getAttribute("Value"));
+
+		// ssn
+		WebElement sSN = driver.findElement(By.id("ssn"));
+		int ssnsendvalue = rd.nextInt(999999999);
+		sSN.sendKeys("" + ssnsendvalue);
+		hMap.put("SSN", sSN.getAttribute("Value"));
+
+		// Address
+		WebElement address = driver.findElement(By.id("addr"));
+		address.sendKeys("abcd,Main Street");
+		hMap.put("Address", address.getAttribute("Value"));
+
+		// Age
+		WebElement age = driver.findElement(By.id("age"));
+		int agesendvalue = rd.nextInt(99);
+		age.sendKeys("" + agesendvalue);
+		hMap.put("Age", age.getAttribute("Value"));
+
+		// weight
+		WebElement weight = driver.findElement(By.id("weight"));
+		int weightsendvalue = rd.nextInt(99);
+		weight.sendKeys("" + weightsendvalue);
+		hMap.put("Weight", weight.getAttribute("Value"));
+
+		// height
+		WebElement height = driver.findElement(By.id("height"));
+		int heightsendvalue = rd.nextInt(99);
+		height.sendKeys("" + heightsendvalue);
+		hMap.put("Height", height.getAttribute("Value"));
+
+		// City
+		WebElement city = driver.findElement(By.id("city"));
+		city.sendKeys("Tampa");
+		hMap.put("City", city.getAttribute("Value"));
+
+		// Stete
+		WebElement state = driver.findElement(By.id("state"));
+		state.sendKeys("Florida");
+		hMap.put("State", state.getAttribute("Value"));
+
+		// Zipcode
+		WebElement zipcode = driver.findElement(By.id("city"));
+		int zip = rd.nextInt(99999);
+		zipcode.sendKeys("" + zip);
+		hMap.put("Zipcode", zipcode.getAttribute("Value"));
+
+		// Edit/SAve Button
+		WebElement edit = driver.findElement(By.id("Ebtn"));
+		edit.click();
+		Thread.sleep(2000);
 		
-	
-	driver.findElement(By.xpath("//span[contains(text(),'Schedule Appointment')]")).click();
-    Thread.sleep(3000);
-    driver.findElement(By.xpath("//input[@type='submit']")).click();
-    Thread.sleep(5000);
-   
-    driver.findElement(By.xpath("//h4[contains(text(),'Dr.Beth')]//ancestor::ul/following-sibling::button")).click();
-    System.out.println(driver.getTitle());
-    WebElement frameEle = driver.findElement(By.id("myframe"));
-    driver.switchTo().frame(frameEle);
-    driver.findElement(By.id("datepicker")).click();
-    driver.findElement(By.xpath("//span[text()='Next']")).click();
-    driver.findElement(By.linkText("2")).click();
-    Thread.sleep(3000);
-    Select select = new Select(driver.findElement(By.id("time")));
-    select.selectByIndex(2);
-//    List<WebElement> options = select.getOptions();
-//    System.out.println(options);
-//    options.get(2).click();
-    Thread.sleep(3000);
-    driver.findElement(By.id("ChangeHeatName")).click();
-    System.out.println(driver.getTitle());
-    Thread.sleep(3000);
-    driver.switchTo().defaultContent();
-    driver.findElement(By.id("sym")).sendKeys("Fever");
-    driver.findElement(By.xpath("//input[@value='Submit']")).click();
-    System.out.println(driver.getTitle());
-    Thread.sleep(3000);
-    System.out.println("Table Row Contents" + driver.findElement(By.xpath("//table/tbody/tr[1]")).getText());
-    Thread.sleep(3000);
-    driver.quit();
+		WebElement save = driver.findElement(By.id("Sbtn"));
+		save.click();
+		
+		driver.switchTo().alert().accept();
+
+
 	}
 
 }
--- Project_MMP/src/test/java/org/iit/mmp/tests/ProfileTest.java
+++ Project_MMP/src/test/java/org/iit/mmp/tests/ProfileTest.java
@@ -10,17 +10,20 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class ProfileTest {
 	WebDriver driver;
-	@Test(description="US_001 Validating the ProfileTab on the Page")
-	public void profile() throws Exception {
-		WebDriverManager.chromedriver().setup();		
-		driver = new ChromeDriver();
-		
-		driver.get("http://96.84.175.78/MMP-Release2-Integrated-Build.6.8.000/portal/login.php");
-		
-		LoginPage lp =new LoginPage(driver);
-		lp.login("ria1","Ria12345");
-		ProfilePage pf = new ProfilePage(driver);
-		pf.profileTab();
+	@Test
+	
+	public void profile() throws InterruptedException{
+	
+	WebDriverManager.chromedriver().setup();		
+	driver = new ChromeDriver();
+	
+	driver.get("http://96.84.175.78/MMP-Release2-Integrated-Build.6.8.000/portal/login.php");
+	
+	LoginPage lp =new LoginPage(driver);
+	lp.login("ria1","Ria12345");
+	
+	
+	ProfilePage pp = new ProfilePage(driver);
+	pp.profile();
 	}
-
 }
