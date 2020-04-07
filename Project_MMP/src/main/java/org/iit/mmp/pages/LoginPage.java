9c75a1515787bff8e1f89ada48dff1c996335c70
siriseleniun <sirishakotha9@gmail.com>
2020-04-07 00:38:11 -0700

    ViewHistory files

--- Project_MMP/src/main/java/org/iit/mmp/pages/LoginPage.java
+++ Project_MMP/src/main/java/org/iit/mmp/pages/LoginPage.java
@@ -1,7 +1,13 @@
 package org.iit.mmp.pages;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class LoginPage {
 WebDriver driver;
@@ -9,6 +15,15 @@ WebDriver driver;
 	public LoginPage(WebDriver driver) {
 		
 		this.driver= driver;
+	}
+	
+	public void patientUrl() {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+
+		driver.get("http://96.84.175.78/MMP-Release2-Integrated-Build.6.8.000/portal/login.php");
+
 	}
 	
 	public void login(String userName, String password) {
@@ -20,4 +35,25 @@ WebDriver driver;
 			driver.findElement(By.name("submit")).click();
 			
 		}
+	public void navigateToModule(String modulename) {
+		
+		driver.findElement(By.xpath("//span[contains(text(),'" + modulename + "')]")).click();
+	}
+	public void viewHistory() {
+		driver.findElement(By.xpath("//button[contains(text(),'View History')]")).click();
+		driver.findElement(By.xpath("//p//a//input[@value='Past Prescription']")).click();
+		List<WebElement> list = driver.findElements(By.xpath("//table//ul//li"));
+		for(int i=0;i<list.size();i++)
+		{
+			//list.get(i).click();
+			System.out.println("The Value of " + i +" is :" +list.get(i).getText());
+
+			
+		}
+		
+	}
+
+	public void logout() {
+		driver.quit();
+	}
 }
--- Project_MMP/src/test/java/org/iit/mmp/tests/LoginTest.java
+++ Project_MMP/src/test/java/org/iit/mmp/tests/LoginTest.java
@@ -2,21 +2,20 @@ package org.iit.mmp.tests;
 
 import org.iit.mmp.pages.LoginPage;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class LoginTest {
 	WebDriver driver;
-	@Test(description="US_001 Validating the Login Page")
+
+	@Test(description = "US_001 Validating the Login Page for ViewHistory")
 	public void login() {
-		WebDriverManager.chromedriver().setup();		
-		driver = new ChromeDriver();
-		
-		driver.get("http://96.84.175.78/MMP-Release2-Integrated-Build.6.8.000/portal/login.php");
-		
-		LoginPage lp =new LoginPage(driver);
+
+		// calling Patient login page and its methods
+		LoginPage lp = new LoginPage(driver);
+		lp.patientUrl();
 		lp.login("ria1","Ria12345");
+		//lp.login("summer", "Test123@");
+		lp.navigateToModule("Profile");
+		lp.viewHistory();
 	}
 }
